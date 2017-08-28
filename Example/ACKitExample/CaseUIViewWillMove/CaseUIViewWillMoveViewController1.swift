362890cf39ff502a00406a3329ef6e9affbb5873
Austin Chen <austin@accodeworks.com>
2017-08-27 21:48:30 -0400

    Fix: CaseUIViewWillMove

--- Example/ACKitExample/CaseUIViewWillMove/CaseUIViewWillMoveViewController1.swift
+++ Example/ACKitExample/CaseUIViewWillMove/CaseUIViewWillMoveViewController1.swift
@@ -20,4 +20,28 @@ class CaseUIViewWillMoveViewController1: UIViewController {
         view = CaseWillMoveView()
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print("viewDidDisappear")
+    }
+
 }
