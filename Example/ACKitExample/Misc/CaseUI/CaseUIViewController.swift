bd1cd3029195643d831c99abffad207c71447edc
Austin Chen <austin.zm@gmail.com>
2018-03-05 19:39:36 -0500

    Fix: minor

--- Example/ACKitExample/Misc/CaseUI/CaseUIViewController.swift
+++ Example/ACKitExample/Misc/CaseUI/CaseUIViewController.swift
@@ -13,6 +13,9 @@ class CaseUIViewController: UIViewController {
     @IBAction func toggleAlertButtonTapped(_ sender: Any) {
         retractableView.setIsHidden(!retractableView.isHidden, animated: true)
     }
+    @IBAction func buttonTapped(_ sender: Any) {
+        performSegue(withIdentifier: "kac", sender: nil)
+    }
     @IBOutlet weak var retractableView: ACRetractableView!
     
     override func viewDidLoad() {
@@ -20,5 +23,16 @@ class CaseUIViewController: UIViewController {
         
         // set up
         retractableView.isHidden = true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        if #available(iOS 11, *) {
+            print("ac1: \(view.safeAreaInsets)") // affected by navigation bar
+            
+            // not affected by any navigation bars / toolbars / tab bars. All non-notch devices are guaranteed to have zero safeAreaInsets at the screen level.
+            print("ac2: \(sharedAppDelegate.window?.safeAreaInsets)")
+        }
     }
 }
